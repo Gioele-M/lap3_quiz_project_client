2eed5a8ebec4b9eb6955c09a299bf04fe5cc4ae6
alicekres <alice.kreslins@gmail.com>
2022-06-27 14:00:13 +0100

    merge solved, in App.js, saved

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchResults } from './actions';
-
-function App() {
-    const dispatch = useDispatch();
-    const error = useSelector((state) => state.error);
-
-    const fetchQuestions = () => {
-        dispatch(fetchResults());
-    };
-=======
 import { useDispatch } from 'react-redux';
 
-import { fetchResults } from "../src/actions";
+import { fetchResults } from '../src/actions';
 
 function App() {
     const dispatch = useDispatch();
     const searchResult = () => dispatch(fetchResults());
->>>>>>> main
 
     return (
         <>
             <h1>Quiz game</h1>
-<<<<<<< HEAD
-            <button onClick={fetchQuestions}>lol</button>
-=======
             <button onClick={searchResult}></button>
->>>>>>> main
         </>
     );
 }
