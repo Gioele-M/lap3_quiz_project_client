ce7f93d79e990e4fe15f8f5fb62871ca2c17c53e
Nadir <nadirkhan98@outlook.com>
2022-06-28 18:03:03 +0100

    fixing test structures

--- src/pages/Quiz/Quiz.test.js
+++ src/pages/Quiz/Quiz.test.js
@@ -1,27 +1,33 @@
-import { default as Quiz } from '.';
-import { screen, render } from '@testing-library/react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { default as Quiz } from ".";
+import { screen, render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../../store";
+import "@testing-library/jest-dom";
 
-describe('Quiz', () => {
-
-    test('it renders', () => {
-        render(
-        <Router >
-            <Quiz />
+describe("Quiz", () => {
+  test("it renders", () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <Quiz />
         </Router>
-        )
-        const heading = screen.getByRole('heading');
-        expect(heading.textContent).toMatch(/quiz/i);
-    });
-    test('it renders', () => {
-        render(
-        <Router >
-           <Quiz /> 
+      </Provider>
+    );
+    const heading = screen.getByRole("heading");
+    expect(heading.textContent).toMatch(/quiz/i);
+  });
+  test("it renders", () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <Quiz />
         </Router>
-        )
-        const heading = screen.getByRole('button', { 
-            name: /Finish quiz/i 
-          });
-        expect(heading).toBeInTheDocument();
+      </Provider>
+    );
+    const heading = screen.getByRole("button", {
+      name: /Finish quiz/i,
     });
+    expect(heading).toBeInTheDocument();
+  });
 });
