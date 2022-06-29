1e6a96a32a13c56bcf351779dd2649b4f4e575b7
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-06-29 16:51:02 +0100

    add player scores and turns to quiz form

--- src/reducers/quizReducer.js
+++ src/reducers/quizReducer.js
@@ -1,11 +1,16 @@
 const initialState = {
     results: [],
+    quiz: [],
     loading: false,
     players: 0,
     amount: 0,
     category: 0,
     categoryDesc: "",
-    difficulty: ""
+    difficulty: "",
+    playerOneScore: 0,
+    playerTwoScore: 0,
+    playerThreeScore: 0,
+    playerFourScore: 0
 };
 
 const quizReducer = (state = initialState, action) => {
@@ -30,6 +35,14 @@ const quizReducer = (state = initialState, action) => {
             return { ...state, amount: action.payload };
         case 'SET PLAYERS':
             return { ...state, players: action.payload };
+        case 'SET PLAYER ONE SCORE':
+            return { ...state, playerOneScore: action.payload };
+        case 'SET PLAYER TWO SCORE':
+            return { ...state, playerTwoScore: action.payload };
+        case 'SET PLAYER THREE SCORE':
+            return { ...state, playerThreeScore: action.payload };
+        case 'SET PLAYER FOUR SCORE':
+            return { ...state, playerFourScore: action.payload };
         case 'SET ERROR':
             return {
                 ...state,
