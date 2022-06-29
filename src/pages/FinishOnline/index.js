21f6f8d72d135ba956b6a1f91a04d6aa3cd408ef
Gioele-M <gioelemontis97@gmail.com>
2022-06-30 00:38:44 +0100

    when completed quiz emit is sent to socket that informs everyone in the room another person has completed the quiz

--- src/pages/FinishOnline/index.js
+++ src/pages/FinishOnline/index.js
@@ -2,11 +2,35 @@ import React from 'react'
 import { useNavigate } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
+
+import socket from '../../actions/socket';
+
 const FinishOnline = () => {
     let navigate = useNavigate();
 
     const playerScore = useSelector((state) => state.playerOneScore);
     const questionsAmount = useSelector((state) => state.amount);
+
+    // USE EFFECT??
+
+    // This will send (... , loggedUser, result)
+    socket.emit('finishGame', 'Game is completed yay')
+
+    // Socket.emit score and amount + player name (useEffect?)
+
+    // think how to pass the room to this
+
+    // set localstate?
+
+
+
+    // Listen to how many players completed the quiz
+
+    socket.on('playerHasCompleted', msg=>{
+        console.log(msg)
+    })
+
+
 
 
     console.log(playerScore, questionsAmount)
@@ -20,3 +44,9 @@ const FinishOnline = () => {
 }
 
 export default FinishOnline;
+
+
+
+
+
+/// Send message to socket -> socket stores all scores -> sends back scores once all scores are in
