3b25a1fbc59d963a63b0176cbe7b2b9ae72e2dff
Gioele-M <gioelemontis97@gmail.com>
2022-06-29 14:15:26 +0100

    connection works

--- src/App.js
+++ src/App.js
@@ -20,35 +20,72 @@ import './pages/Difficulty/style.css';
 import './pages/Category/style.css';
 import './components/HighscoreModal/style.css';
 
+// function App() {
+//     // const dispatch = useDispatch();
+//     const loading = useSelector((state) => state.loading);
+//     console.log(loading);
+//     // const searchResult = () => dispatch(fetchResults());
+
+//     return (
+//         <>
+//             <Routes>
+//                 <Route index element={<Login />} />
+//                 <Route path="home" element={<Home />} />
+//                 <Route path="numplayers" element={<NumPlayers />} />
+//                 <Route path="category" element={<Category />} />
+//                 <Route path="difficulty" element={<Difficulty />} />
+//                 <Route path="quiz" element={<Quiz />} />
+//                 <Route path="finish" element={<Finish />} />
+//                 <Route path="register" element={<Register />} />
+//             </Routes>
+//             {/* <button onClick={searchResult}></button>
+
+//             {/* {loading ? (
+//                 <h2>Loading...</h2>
+//             ) : (
+//                 <>
+//                     <h1>Quiz game</h1>
+//                     <button onClick={searchResult}>Search results</button>
+//                 </>
+//             )} */}
+//         </>
+//     );
+// }
+
+
+import io from "socket.io-client";
+const serverEndpoint = "http://localhost:3000/";
+
 function App() {
-    // const dispatch = useDispatch();
-    const loading = useSelector((state) => state.loading);
-    console.log(loading);
-    // const searchResult = () => dispatch(fetchResults());
+    let state = { socket: null };
+    
+    const socket = io(serverEndpoint);
+
+    socket.on('connect', () => {console.log('Connected with id'+socket.id)})
+
+    socket.on('admin-message', msg => console.log(msg));
+    
+    
+    socket.emit('message', 'this is the message')
+
+    
+
+
+    console.log('socket' + socket)
+    console.log(socket)
+    
+    
 
     return (
-        <>
-            <Routes>
-                <Route index element={<Login />} />
-                <Route path="home" element={<Home />} />
-                <Route path="numplayers" element={<NumPlayers />} />
-                <Route path="category" element={<Category />} />
-                <Route path="difficulty" element={<Difficulty />} />
-                <Route path="quiz" element={<Quiz />} />
-                <Route path="finish" element={<Finish />} />
-                <Route path="register" element={<Register />} />
-            </Routes>
-            {/* <button onClick={searchResult}></button>
-
-            {/* {loading ? (
-                <h2>Loading...</h2>
-            ) : (
-                <>
-                    <h1>Quiz game</h1>
-                    <button onClick={searchResult}>Search results</button>
-                </>
-            )} */}
-        </>
-    );
+            <div id="App">Hi</div>
+        );
 }
+
+
+
+
+
+
+
+
 export default App;
