f8ac9f3886654ff88c1fa29a0d804ee6788d738f
alicekres <alice.kreslins@gmail.com>
2022-06-30 17:46:49 +0100

    remove console.logs from LogoutButton, remove wrong import for useDispatch

--- src/components/LogoutButton/index.js
+++ src/components/LogoutButton/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const LogoutButton = () => {
-    const user = useSelector((state) => state.user);
-    const results = useSelector((state) => state.results);
     const dispatch = useDispatch();
 
     let navigate = useNavigate();
 
-    console.log('BEFORE', user);
-    console.log('BEFORE', results);
     const handleSignout = () => {
         dispatch({ type: 'LOAD_RESULTS', payload: [] });
         dispatch({ type: 'SET USER', payload: '' });
         navigate('/');
     };
-    console.log('AFTER', results);
-    console.log('AFTER', user);
 
     return <button onClick={handleSignout}>Sign out</button>;
 };
