1810a01cfc7a08404059c1b6f488580a85ddf4d0
alicekres <alice.kreslins@gmail.com>
2022-06-30 13:23:40 +0100

    adding for loops to code so that stringify method doesn't have to bu used

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,13 +22,31 @@ export const fetchResults = (amount, category, difficulty) => {
                 `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`
             );
             const results = data.results;
-            const rawresultsToString = JSON.stringify(results);
-            const decodedStringResults = decode(rawresultsToString);
-            const decodedResults = JSON.parse(decodedStringResults);
+            console.log('Before', results);
+            for (let i = 0; i < results.length; i++) {
+                let instance = results[i];
+                let newQuestion = decode(instance.question);
+                instance.question = newQuestion;
+                let newCorrectAnswer = decode(instance.correct_answer);
+                instance.correct_answer = newCorrectAnswer;
+                for (let j = 0; j < results[i].incorrect_answers.length; j++) {
+                    let array = results[i].incorrect_answers[j];
+                    let newWrongAnswer = decode(array.incorrect_answer);
+                    instance.incorrect_answer = newWrongAnswer;
+                }
+            }
+            console.log('AFTER', results);
+            // const rawresultsToString = JSON.stringify(results);
+            // console.log('RAW RESULTS', rawresultsToString);
+            // const decodedStringResults = decode(rawresultsToString);
+            // console.log('DECODED', decodedStringResults);
+            // console.log(typeof decodedStringResults);
+            // const decodedResults = JSON.parse(decodedStringResults);
+            // console.log(decodedResults);
 
             dispatch({
                 type: 'LOAD_RESULTS',
-                payload: decodedResults,
+                payload: results,
             });
         } catch (err) {
             dispatch({
